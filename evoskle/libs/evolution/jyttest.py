a5cea67c3c4935e9ffc9a16a83ffa14daf19d1c2
jiangyutong <1834921609@qq.com>
2022-06-13 19:09:24 +0800

    evosklecopy

--- evoskle/libs/evolution/jyttest.py
+++
@@ -1,7 +0,0 @@
-import  torch
-import numpy as np
-cascade = np.load('../data/human3.6M/h36m_annot_train.npy',allow_pickle=True)
-print(cascade)
-# len = 10
-# for  i in range(len):
-#     print(i)
--- evoskle/tools/get2d.py
+++
@@ -1,7 +0,0 @@
-import libs.dataset.h36m.data_utils2 as data_utils
-import libs.parser.parse as parse
-
-opt = parse.parse_arg()
-if __name__ == '__main__':
-    train_dataset, eval_dataset, stats, action_eval_list = \
-        data_utils.prepare_dataset(opt)
